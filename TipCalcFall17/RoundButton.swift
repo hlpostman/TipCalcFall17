93018c1267973c593ec7415ea495450ecef00b6b
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-05 11:00:04 -0800

    custom roundButton class with IBDesignable and IBInspectable properties

---
+++ TipCalcFall17/RoundButton.swift
@@ -0,0 +1,26 @@
+//
+//  RoundButton.swift
+//  TipCalcFall17
+//
+//  Created by Aristotle on 2017-11-05.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var myBackgroundColor: UIColor? {
+        didSet {
+            self.backgroundColor = myBackgroundColor
+        }
+    }
+
+}
