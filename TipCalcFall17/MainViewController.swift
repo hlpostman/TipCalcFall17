c484db86f9a3ff56b2e3a8fcd9164851f8cd2d00
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-05 17:28:19 -0800

    all input buttons 0-9 and "." type into bill label correctly

--- TipCalcFall17/MainViewController.swift
+++ TipCalcFall17/MainViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var tipLevel = 0.20
+    
+
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    var tipAmount: Double {
+        let bill = Double(billLabel.text ?? "") ?? 0
+        let tip = bill * tipLevel
+        return tip
+    }
+    
+    func showTip() {
+        tipLabel.text = String(tipAmount)
+    }
+    
+
+    @IBAction func inputBill(_ sender: UIButton) {
+    
+        billLabel.text = billLabel!.text! + sender.titleLabel!.text!
+        print(billLabel.text!)
+    }
+    
+    func deleteBillInput() {
+//        if "0.123456789".range(of: billLabel!.text!.endIndex as! String) != nil {
+//            let lastTyped = self.billLabel!.text!.endIndex
+//            self.billLabel!.text! = billLabel!.text![..<lastTyped] as! String
+//        }
+        print("This function will allow a user to delete input as they type the bill in case they make an error.  NOT YET IMPLEMENTED.")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,11 +57,11 @@ class MainViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     let settingsViewController = segue.destination as! SettingsViewController
-     settingsViewController.mainScreenView = view
-     }
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ let settingsViewController = segue.destination as! SettingsViewController
+ settingsViewController.mainScreenView = view
+ }
 
 }
