a6fb82d2293998ae86a04886e320232ef1555c35
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-05 21:39:40 -0800

    Worked on label title animations.  Commented out currently and will adjust story board for interim.

--- TipCalcFall17/MainViewController.swift
+++ TipCalcFall17/MainViewController.swift
@@ -12,30 +12,34 @@ import UIKit
 class MainViewController: UIViewController {
 
     // Labels
-    @IBOutlet weak var billLabelField: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billLabelTitle: UILabel!
+    @IBOutlet weak var tipLabelTitle: UILabel!
+    @IBOutlet weak var totalLabelTitle: UILabel!
+    lazy var labelTitleDictionary = [billLabelTitle: billLabel, tipLabelTitle: tipLabel, totalLabelTitle: totalLabel]
     
-    // Animation
-    var labelAnimation: UISnapBehavior!
-    var labelAnimator: UIDynamicAnimator!
-    
-    // Variables
+    // General Variables
     var tipLevel = 0.20
     
     var tipAmount: Double {
-        let bill = Double(billLabelField.text ?? "") ?? 0
+        let bill = Double(billLabel.text ?? "") ?? 0
         let tip = bill * tipLevel
         return tip
     }
     
     var totalBill: Double {
-        let bill = Double(billLabelField.text ?? "") ?? 0
+        let bill = Double(billLabel.text ?? "") ?? 0
         let totalBill = bill + self.tipAmount
         return totalBill
     }
     
-    // Functions
+    // Animation variables
+    var labelAnimation: UISnapBehavior!
+    var labelAnimator: UIDynamicAnimator!
+    
+    // General Functions
     func showTip() {
         tipLabel!.text! = String(tipAmount)as String!
     }
@@ -46,24 +50,63 @@ class MainViewController: UIViewController {
 
     @IBAction func inputBill(_ sender: UIButton) {
     
-        billLabelField.text = billLabelField!.text! + sender.titleLabel!.text!
+        minimizeLabelTitles()
+        billLabel.text = billLabel!.text! + sender.titleLabel!.text!
         showTip()
         showTotal()
-        print(billLabelField.text!)
+        print(billLabel.text!)
     }
     
     func deleteBillInput() {
-//        if "0.123456789".range(of: billLabelField!.text!.endIndex as! String) != nil {
-//            let lastTyped = self.billLabelField!.text!.endIndex
-//            self.billLabelField!.text! = billLabelField!.text![..<lastTyped] as! String
+//        if "0.123456789".range(of: billLabel!.text!.endIndex as! String) != nil {
+//            let lastTyped = self.billLabel!.text!.endIndex
+//            self.billLabel!.text! = billLabel!.text![..<lastTyped] as! String
 //        }
         print("This function will allow a user to delete input as they type the bill in case they make an error.  NOT YET IMPLEMENTED.")
     }
+    
+    // Animation Functions
+    func minimizeLabelTitles () {
+        // Not working.  Should work in conjuction with an enlargeLabelTitles() function
+        // to show a title in large text on the left-hand side of the label that will
+        // show the bill/tip/total when no bill is entered, and then move that title
+        // to the upper left-hand corner in small text when bill != nil.  An observer
+        // may be useful. 11/5/17 HLP
+        print("minimizeLabelTitles() not yet implemented.")
+        // Debug
+//        print("Attempting to minimizeLabelTitles()")
+//        labelAnimator.removeAllBehaviors()
+//
+//        for (titleLabel, parentView) in labelTitleDictionary {
+            // Shrink font size
+//            The below not happening, though not causing a crash
+//            let currentFontSize = titleLabel.font.pointSize
+//            titleLabel.font.withSize(currentFontSize/2)
+//            print(titleLabel.text!)
+            
+            // Move up
+//            let destinationPoint = CGPoint(x: parentView!.frame.minX, y: parentView!.frame.minY)
+//            let goUp = UISnapBehavior(item: titleLabel, snapTo: destinationPoint)
+//            labelAnimator.addBehavior(goUp)
+//            Alternatives that don't work:
+//            let destinationPoint = CGPoint(x: 20, y: 20)
+//            titleLabel.frame.offsetBy(dx: 10.0 as! CGFloat, dy: 10.0 as! CGFloat)
+//
+//        }
+        // Debug
+//        print("Ran minimizeLabelTitles() 🌸")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Animation
+        labelAnimator = UIDynamicAnimator(referenceView: billLabel)
+        
+        // Maintain constraints
+        self.view.layoutIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
